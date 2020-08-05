e93cd97b3eed10c53749c2e90549d5904353c3ba
Margarita Ganeva <rita.ganeva@gmail.com>
2020-08-05 20:16:43 +1000

    Normalized sounds

--- sounds.lua
+++ sounds.lua
@@ -1,6 +1,5 @@
 sounds = {}
 sounds.scissors = love.audio.newSource("asset/scissors.wav", "static")
-sounds.scissors:setVolume(0.5)
 sounds.waterHit = love.audio.newSource("asset/water-hit.wav", "static")
 sounds.wallHit = love.audio.newSource("asset/wall-hit.wav", "static")
 sounds.buttonPress = love.audio.newSource("asset/button-press.wav", "static")
