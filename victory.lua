ee5d297d31efe505ad960908d464fd3d0e295b7a
Margarita Ganeva <rita.ganeva@gmail.com>
2020-09-13 22:39:30 +1000

    Show a screenshot of game state on victory

--- victory.lua
+++ victory.lua
@@ -8,7 +8,7 @@ function Victory:init()
   self.victory_anim = Anim8.newAnimation(grid('1-5', 1), 0.5, "pauseAtEnd")
 end
 
-function Victory:enter(from, level)
+function Victory:enter(from, level, screenshot)
   Buttons = B:new()
   Buttons:add("continue", 542, 350, "Continue", false, Info, level)
   Buttons:setActive(Buttons.continue)
@@ -17,6 +17,9 @@ function Victory:enter(from, level)
   else
     sounds.victory:play()
   end
+  self.victory_anim:pauseAtStart()
+  self.victory_anim:resume()
+  self.screenshot = screenshot
 end
 
 function Victory:update(dt)
@@ -24,11 +27,15 @@ function Victory:update(dt)
 end
 
 function Victory:leave()
+  self.screenshot = nil
   Buttons:clear()
 end
 
 function Victory:draw()
+  love.graphics.draw(self.screenshot, 0, 0)
   resetToDraw()
+  love.graphics.setColor(1,1,1, 0.7)
+  love.graphics.rectangle('fill', 0, 0, ORIGINAL_GAME_X, ORIGINAL_GAME_Y)
   love.graphics.setColor(1,1,1, 1)
   self.victory_anim:draw(sprites.playerVictory, 576, 200)
   Buttons:draw()
