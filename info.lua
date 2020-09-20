8a33b7736c73fdbe69581380bcf6ef179684a93c
Margarita Ganeva <rita.ganeva@gmail.com>
2020-09-20 11:16:23 +1000

    Add more credits

--- info.lua
+++ info.lua
@@ -2,6 +2,8 @@ local Info = {}
 local Buttons
 local width = 680
 local baseY
+local namesCount = 13
+local namesHeight = 20
 
 function Info:init()
   self.level = 0
@@ -11,6 +13,11 @@ function Info:init()
   self.stroke = love.graphics.newImage('asset/info3.png')
   self.logo = love.graphics.newImage('asset/logo.png')
   self.ravnecLogo = love.graphics.newImage('asset/ravnec-logo.png')
+  self.tiledLogo = love.graphics.newImage('asset/tiled-logo.png')
+  self.pyxelLogo = love.graphics.newImage('asset/pyxel-edit-logo.png')
+  self.loveLogo = love.graphics.newImage('asset/love2d-logo.png')
+  self.luaLogo = love.graphics.newImage('asset/lua-logo.png')
+  self.martenitsaLogo = love.graphics.newImage('asset/martenitsa-logo.png')
 end
 
 function Info:enter(previous, level)
@@ -36,7 +43,7 @@ function Info:leave()
 end
 
 function Info:update(dt)
-  if self.level == 8 then
+  if self.level == 8 and baseY > -(970 + namesCount*namesHeight) then
     baseY = baseY - dt*40
   end
 end
@@ -54,6 +61,11 @@ function Info:draw()
   elseif self.level == 8 then
     love.graphics.draw(self.logo, getCenteredHorizontalPosition(64), baseY + 95)
     love.graphics.draw(self.ravnecLogo, getCenteredHorizontalPosition(126), baseY + 210)
+    love.graphics.draw(self.pyxelLogo, getCenteredHorizontalPosition(179), baseY + namesCount*namesHeight + 430)
+    love.graphics.draw(self.tiledLogo, getCenteredHorizontalPosition(184), baseY + namesCount*namesHeight + 575)
+    love.graphics.draw(self.luaLogo, getCenteredHorizontalPosition(100), baseY + namesCount*namesHeight + 710)
+    love.graphics.draw(self.loveLogo, getCenteredHorizontalPosition(129), baseY + namesCount*namesHeight + 850)
+    love.graphics.draw(self.martenitsaLogo, getCenteredHorizontalPosition(375), baseY + namesCount*namesHeight + 1250)
   end
 
   love.graphics.setColor(0, 0, 0)
@@ -112,13 +124,16 @@ function Info:draw()
     love.graphics.printf("Thank you for playing Martenitsa!", getCenteredHorizontalPosition(width), baseY, width, "center")
     love.graphics.printf("Developed by Margarita Ganeva", getCenteredHorizontalPosition(width), baseY + 60, width, "center")
     love.graphics.printf("Music by Ravnec Folklore Band", getCenteredHorizontalPosition(width), baseY + 180, width, "center")
-    love.graphics.printf("Big thanks to", getCenteredHorizontalPosition(width), baseY + 320, width, "center")
+    love.graphics.printf("Big thanks to", getCenteredHorizontalPosition(width), baseY + 325, width, "center")
     love.graphics.setFont(fonts.creditsNames)
-    love.graphics.printf("Veliko Dragiev\nBerend Krebs\nIvan Kasarov\nLyubomir Gizdov\nStefan Manolov\nAnna Velcheva\nDimitar Panov\nKristina Stoyanova\nPeko Atanasov\nIva Popova\nKristina Stefanova\nStanislav Bozhkov\n", getCenteredHorizontalPosition(width), baseY + 350, width, "center")
+    love.graphics.printf("Veliko Dragiev\nBerend Krebs\nIvan Kasarov\nLyubomir Gizdov\nStefan Manolov\nAnna Velcheva\nDimitar Panov\nBogdan Kurtev\nKristina Stoyanova\nPeko Atanasov\nIva Popova\nKristina Stefanova\nStanislav Bozhkov\n", getCenteredHorizontalPosition(width), baseY + 355, width, "center")
     love.graphics.setFont(fonts.credits)
-    love.graphics.printf("for the testing, advising and the overall support", getCenteredHorizontalPosition(width), baseY + 600, width, "center")
+    love.graphics.printf("for the testing, advising and the overall support", getCenteredHorizontalPosition(width), baseY + namesCount*namesHeight + 350, width, "center")
+    love.graphics.printf("All graphics are drawn with", getCenteredHorizontalPosition(width), baseY + namesCount*namesHeight + 400, width, "center")
+    love.graphics.printf("The levels are designed using", getCenteredHorizontalPosition(width), baseY + namesCount*namesHeight + 540, width, "center")
+    love.graphics.printf("And the game is written in", getCenteredHorizontalPosition(width), baseY + namesCount*namesHeight + 680, width, "center")
+    love.graphics.printf("with", getCenteredHorizontalPosition(width), baseY + namesCount*namesHeight + 820, width, "center")
   end
-
   love.graphics.setColor(1, 1, 1)
   Buttons:draw()
   drawBorders()
